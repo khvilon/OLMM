9d18f739d7fbbef2f9234d523e577084f49c3b28
Alexander Zhukov <sleeeem@mail.ru>
2015-06-04 18:44:57 +0300

    return standart ol-debug file

--- OpenLayers/build/ol-debug.js
+++ OpenLayers/build/ol-debug.js
@@ -50086,7 +50086,7 @@ ol.events.condition.never = goog.functions.FALSE;
 /**
  * @param {ol.MapBrowserEvent} mapBrowserEvent Map browser event.
  * @return {boolean} True if the browser event is a `pointermove` event.
- * @apimousemove
+ * @api
  */
 ol.events.condition.pointerMove = function(mapBrowserEvent) {
   return mapBrowserEvent.type == 'pointermove';
