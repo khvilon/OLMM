1f2b25e71b68f0537a975ac3bcc607d42598ada6
Alexander Zhukov <sleeeem@mail.ru>
2015-06-05 18:54:11 +0300

    refactoring mmtest ng add features function

--- plugins/mmtest_ng.js
+++ plugins/mmtest_ng.js
@@ -1,41 +1,4 @@
 (function (module) {
-    module.mmTestNextGenAddFeaturesWithFullClean = function(geojsonData, source_name) {
-        this._clearSources();
-        this._addFeatures(geojsonData, source_name, true)
-    };
-
-    module.mmTestNextGenAddFeaturesWithClean = function(geojsonData, source_name) {
-        this._clearSources(source_name);
-        this._addFeatures(geojsonData, source_name, true)
-    };
-
-    module.mmTestNextGenAddDynamicFeatures = function(geojsonData, source_name) {
-        this._addFeatures(geojsonData, source_name, false)
-    };
-
-    module.mmTestDeleteFeatureById = function(feature_id) {
-        var source_name, source, point_feature;
-
-        for (source_name in this.sources) {
-            source = this.getSourceByName(source_name);
-            if (!!source.getFeatures) {
-                point_feature = source.getFeatureById(feature_id);
-
-                source.removeFeature(source.getFeatureById(feature_id));
-            }
-        }
-    };
-
-    module.initHoverOnFeature = function() {
-        var self = this;
-
-        var selectMouseMove = new ol.interaction.Select({
-            condition: ol.events.condition.pointerMove,
-            layers: [self.getLayerByName('main')]
-        });
-
-        //self.map.addInteraction(selectMouseMove);
-    };
 
     module.initLayers = function() {
         var self = this;
@@ -114,32 +77,40 @@
         }
     };
 
-    module._clearSources = function(source_name) {
+    module.initHoverOnFeature = function() {
         var self = this;
 
-        if (source_name) {
-            self.getSourceByName(source_name).clear();
-        } else {
-            for (source_name in self.sources) {
-                var source = self.getSourceByName(source_name);
-                if (!!source.getFeatures) {
-                    source.clear();
-                }
+        var selectMouseMove = new ol.interaction.Select({
+            condition: ol.events.condition.pointerMove,
+            layers: [self.getLayerByName('main')]
+        });
+
+        //self.map.addInteraction(selectMouseMove);
+    };
+
+    module.clearSource = function (source_name) {
+        self.getSourceByName(source_name).clear();
+    };
+
+    module.clearSources = function () {
+        for (var source_name in self.sources) {
+            var source = self.getSourceByName(source_name);
+            if (!!source.getFeatures) {
+                source.clear();
             }
         }
     };
 
-    module._addFeatures = function(geojson_data, source_name, need_fit) {
+    module.mmTestNextGenAddFeatures = function(kwargs_data) {
         var self = this;
+        var geojson_data = kwargs_data['geojson_data'] || [];
+        var source_name = kwargs_data['source_name'] || '';
+        var need_fit = kwargs_data['need_fit'] || false;
 
-        var json_string = JSON.stringify(geojson_data);
-        var geojson = JSON.parse(json_string);
-
+        var geojson = JSON.parse(JSON.stringify(geojson_data));
         var features = self.readGeoJSON(geojson, true);
 
         self.getSourceByName(source_name).addFeatures(features);
-
-        //self.transformPointsToLine(features, 'lines');
 
         if (need_fit){
             self.fitToExtent('main');
