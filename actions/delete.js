330e70780e9f3fad5bb835945845089c09fcb6d5
Alexander Zhukov <sleeeem@mail.ru>
2015-06-10 14:19:14 +0300

    add test7.json to git for testing

--- actions/delete.js
+++ actions/delete.js
@@ -1,13 +1,45 @@
-OLMM.prototype.addDeleteFunction = function () {
-    this.map.on('doubleclick', function(evt){
-        var feature = this.forEachFeatureAtPixel(evt.pixel,
-            function(feature, layer) {
-                return feature;
-            });
-        if (feature) {
-            var id = feature.getId();
-            console.log(feature.get('name'), id);
-            feature.remove();
+OLMM.prototype.deleteFeatureFromLayer = function(event) {
+
+    console.log(layer_name);
+    console.log(event);
+
+    console.log(this);
+
+    var self = this;
+    var layer_name = self._layerForDelete;
+
+    self.disableDeleteModeForAll();
+
+    var source = self.getLayerByName(layer_name).getSource();
+
+    var feature = self.map.forEachFeatureAtPixel(
+        event.pixel,
+        function(feature, layer) {
+            return feature;
         }
-    });
+    );
+
+    console.log(feature);
+
+    if (feature) {
+        var id = feature.getId();
+        console.log(id);
+        source.removeFeature(feature);
+    }
+};
+
+OLMM.prototype.enableDeleteMode = function(layer_name) {
+    this._layerForDelete = layer_name;
+    this.map.addEventListener('singleclick', this.deleteFeatureFromLayer);
+};
+
+OLMM.prototype.disableDeleteMode = function() {
+    this.map.removeEventListener('singleclick', this.deleteFeatureFromLayer);
+};
+
+OLMM.prototype.disableDeleteModeForAll = function() {
+    var self = this;
+    for (layer_name in self.layers) {
+        self.map.removeEventListener('singleclick', self.deleteFeatureFromLayer.bind(self, layer_name));
+    }
 };
