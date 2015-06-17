b4b27b90f075e54d958aa1beb6357fca580b024e
Alexander Zhukov <sleeeem@mail.ru>
2015-06-17 13:29:35 +0300

    init layers for gis

--- plugins/gis.js
+++ plugins/gis.js
@@ -1,3 +1,23 @@
+OLMM.prototype.initGisLayers = function () {
+    var self = this;
+    var layer_name = 'edit';
+    var layer = self.getLayerByName(layer_name);
+
+    if (!layer) {
+        self.addLayer(layer_name, self.createVectorLayer(
+            new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'blue',
+                    width: 3
+                }),
+                fill: new ol.style.Fill({
+                    color: 'blue'
+                })
+            })
+        ));
+    }
+};
+
 OLMM.prototype.getCoordsForRequest = function () {
     var self = this;
     var coords = self.getViewPortCoords();
